4460ad056fc963d68bbcbac822a0f751e6a98844
Claudio Pralon <cpralon.rj@gmail.com>
2026-01-22 21:50:57 -0300

    v0119

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://zoeibulnszdandjkjgcp.supabase.co";
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvZWlidWxuc3pkYW5kamtqZ2NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxMjA0NjAsImV4cCI6MjA4NDY5NjQ2MH0.U9teCpW8J_4AkWQI3aFYRATneH4F5T2yq5HGNixq8v4";
 
 if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error('Missing Supabase Environment Variables');
